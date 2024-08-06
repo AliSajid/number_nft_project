ea44cddf31a33a7e337c602d7683b9ef2603d02c
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-08-06 18:05:32 -0400

    build: update hardhat config
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- number_nft_contracts/hardhat.config.ts
+++ number_nft_contracts/hardhat.config.ts
@@ -2,11 +2,19 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+    solidity: '0.8.24',
+    paths: {
+        sources: './contracts',
+        tests: './test',
+        cache: './cache',
+        artifacts: '../number_nft_frontend/src/lib/contracts'
+    }
 };
 
 export default config;
