721bb9ac138d0e30e7331e48dcb91277d8e4c01d
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-08-06 21:49:32 -0400

    feat: add tests for ColoredNumbers
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -4,8 +4,6 @@
 
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-waffle';
 
 const config: HardhatUserConfig = {
     solidity: '0.8.24',
---
+++ contracts/test/ColoredNumbers.ts
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2024 Ali Sajid Imami
+//
+// SPDX-License-Identifier: MIT
+
+import {
+    time,
+    loadFixture
+} from '@nomicfoundation/hardhat-toolbox/network-helpers';
+import { anyValue } from '@nomicfoundation/hardhat-chai-matchers/withArgs';
+import { expect } from 'chai';
+import hre from 'hardhat';
+
+describe('ColoredNumbers', function () {});
